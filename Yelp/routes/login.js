90bc3b7167cb5381f33903e171cd8920098cefea
siyang <siyangshu@gmail.com>
2015-04-04 18:02:35 -0400

    new feature by lulu: login page, user page

--- Yelp/routes/login.js
+++ Yelp/routes/login.js
@@ -7,7 +7,40 @@
 //});
 
 var APP_USERS = require('../lib/table').APP_USERS;
+var database = require('../lib/database');
+var requestQueryParser = require('../lib/requestQueryParser');
+var getData = require('../lib/row.js').getData;
 
+function post(request, response) {
+	
+	console.log("login post");
+	var row = requestQueryParser.parse(request, ["USER_NAME_ID", "PASSWORD"]);
+	var exists;
+	database.exist(APP_USERS, row, function(err, bool) {
+		if (err === null) {
+			console.log(bool);
+			if (bool) {
+				console.log("log success");
+				// successfully registered. redirect to homepage
+				response.writeHead(302, {
+					  'Location': '/user/' + getData(row, 'USER_NAME_ID')
+				});
+				response.end();		
+			} else {
+				response.render('login.jade', {
+					message: 'Incorrect username and password combination',
+					schema: APP_USERS.schema,
+					label: APP_USERS.label
+				});
+			}
+		}
+	});
+	//console.log(exists);
+	
+}
+
+
+exports.post = post;
 exports.do_work = function(req, res){
 	  res.render('login.jade', { 
 		  title: 'hello',

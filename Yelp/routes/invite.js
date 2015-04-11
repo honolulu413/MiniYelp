c04ab32476f9a31a67b46e4ff25a4dba9227f51d
honolulu413 <honolulu413beiyong@gmail>
2015-04-11 15:41:25 -0400

    -m 'invite.js invite.jade added' Lu

--- Yelp/routes/invite.js
+++ Yelp/routes/invite.js
@@ -5,16 +5,59 @@ var getPath = require('../lib/string').getPath;
 exports.post = function(req, res){
 //	var row = requestQueryParser.parse(req, ["current_user", "friends_list"]);
 	var userName = req.session.username;
+	var batch = [];
+	var userIDs = req.param('friends_list');
+	var num = userIDs.length;
+	for (var i = 0; i < userIDs.length; i++) {
+		var query = "SELECT BUSINESS_ID FROM FAVORITES WHERE " +
+				"USER_NAME_ID = " + "'" +  userIDs[i] + "'";
+		batch.push(query);
+	}
 	
-	var values1 = req.param('friends_list')[0];
-	var values2 = req.param('friends_list')[1];
-
-	console.log(values1);
-	console.log(values2);
-
-
-	console.log("invite name is " + userName);
-
+	database.executeBatch(batch, function(err, results) {
+		if (err === null) {
+			console.log(results);
+			var rank = {};
+			for (var result in results) {
+				for (var business in result) {
+					var businessID = business["BUSINESS_ID"];
+					if (typeof rank[businessID] !== 'undefined') {
+						rank[businessID]['number'] += 1;
+					} else {
+						rank[businessID] = {
+							'business': business,
+							'number': 1
+						}
+					}
+				}
+			}
+			console.log("rank");
+			console.log(rank);
+			var rankedArray = $.map(rank, function(value, index) {
+			    return [value];
+			});
+			
+			var sorted = rankedArray.slice().sort(function(a, b){return b.number-a.number});
+			console.log("sorted");
+			console.log(sorted);
+			
+			var finalResult = [];
+			for (var entry in sorted) {
+				var business = entry['business'];
+				finalResult.push(business);
+			}
+			
+			res.render('invite.jade', {	
+				'business_list': finalResult
+			});
+			
+//			console.log(results[0]);
+//			console.log(results[1]);
+//			console.log(results[0].length);
+//			console.log(results[1].length);
+//			console.log(results[2]);
+		}
+	});	
 	
 	
 	

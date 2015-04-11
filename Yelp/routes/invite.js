7c65148b0419adb33306d8e944f3aa20b95e7bed
JoeJoe1989 <josephzhou2011@gmail.com>
2015-04-11 15:24:23 -0400

    user jade friends invite party and href

--- Yelp/routes/invite.js
+++ Yelp/routes/invite.js
@@ -8,12 +8,15 @@ exports.post = function(req, res){
 	
 	var values1 = req.param('friends_list')[0];
 	var values2 = req.param('friends_list')[1];
+	var values3 = req.param('friends_list')[2];
 
-	console.log(values1);
-	console.log(values2);
+	
+//	console.log("value1 is " + values1);
+//	console.log("value2 is " + values2);
+//	console.log("value3 is " + values3);
 
 
-	console.log("invite name is " + userName);
+	console.log("invite name is " + req.param('friends_list'));
 
 	
 	

21e581663c0c048d859d96329603202411349485
siyang <siyangshu@gmail.com>
2015-04-25 17:40:03 -0400

    business.js: if is favorate, do not display "Add to my favorite" button

--- Yelp/routes/business.js
+++ Yelp/routes/business.js
@@ -26,7 +26,6 @@ function get(request, respond) {
                 businessState = businessInfo["STATE"];
                 var batchQuery = [];
                 
-                 
                 var goodReview = "SELECT * FROM (SELECT * FROM REVIEWS WHERE REVIEWS.STAR>=4 AND REVIEWS.BUSINESS_ID="
                         + "'" + businessID + "'"
                         + " ORDER BY REVIEWS.USEFUL_VOTE_NUMBER DESC) WHERE ROWNUM<=3";
@@ -39,9 +38,14 @@ function get(request, respond) {
                         "FROM BUSINESS_CATEGORIES WHERE CATEGORY IN ( SELECT CATEGORY FROM BUSINESS_CATEGORIES WHERE " +
                         "BUSINESS_ID = " + "'" + businessID + "'" + " ) ) AND CITY = " + "'" + businessCity + "'" +" AND " +
                                 "STATE= " + "'" + businessState + "'"  + " ORDER BY STAR DESC) WHERE ROWNUM <=3";
+                
+                var favorateBusinessQuery = " SELECT * FROM FAVORITES  " +
+                					" WHERE USER_NAME_ID = '" + currentUser + "' " + 
+                					" AND BUSINESS_ID = '" +  businessID + "' ";
                 batchQuery.push(goodReview);
                 batchQuery.push(badReview);
                 batchQuery.push(similarBusinesses);
+                batchQuery.push(favorateBusinessQuery);
 
                 database.executeBatch(batchQuery, function(err, resultsArray) {
                     if (err === null) {
@@ -50,17 +54,23 @@ function get(request, respond) {
 						for(var i = 0; i < similarBusinessList.length; i++) {
 							similarBusinessList[i].data[0] = '/business/' + similarBusinessList[i].data[0];
 						}
-						console.log("=============");
-						console.log(similarBusinessList);
 						
                         var businessList = [];
                         businessList.push(businessInfo);
+                        
+                        if (resultsArray[3].length == 0) {
+                        	var isFavorate = false;
+                        } else {
+                        	var isFavorate = true;
+                        }
+                        
                         respond.render('business.jade', {   
                             business: businessList,
                             goodRievew : resultsArray[0],
                             badReview : resultsArray[1],
                             userName: currentUser,
-                            similar_business_list: similarBusinessList
+                            similar_business_list: similarBusinessList,
+                            is_favorate : isFavorate
                         });
                         
                     }

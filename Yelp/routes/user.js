93a5f459caea2c8d48bfdc4a9fa72a543b2a0d6b
siyang <siyangshu@gmail.com>
2015-04-25 17:14:01 -0400

    recommend similar user

--- Yelp/routes/user.js
+++ Yelp/routes/user.js
@@ -6,10 +6,11 @@ var getPath = require('../lib/string').getPath;
 var project = require('../lib/table').project;
 var getData = require('../lib/row').getData;
 var rowArrayWithLabel = require('../lib/row').rowArrayWithLabel;
+var recommend_similar_user = require('../lib/recommend/recommend_similar_user');
+var async = require('async');
 
 function get(request, respond) {
 	var userName = getPath(request.params[0]);
-
 
 	if (!alphanumeric(userName)) {
 		respond.redirect('/login');
@@ -30,66 +31,58 @@ function get(request, respond) {
 
 								console.log("current user is:"
 										+ currentUser.USER_NAME_ID);
-
-//								var similarUserQuery = "SELECT * FROM APP_USERS WHERE LOCATION_CITY = '"
-//										+ currentUser.LOCATION_CITY
-//										+ "' AND LOCATION_STATE = '"
-//										+ currentUser.LOCATION_STATE
-//										+ "'"
-//										+ " AND USER_NAME_ID <> "
-//										+ "'"
-//										+ currentUser.USER_NAME_ID
-//										+ "'"
-//										+ " AND ROWNUM < 3";
+								
 								var favoriteBuziQuery = "SELECT * FROM BUSINESSES WHERE BUSINESSES.BUSINESS_ID IN "
 										+ "( SELECT BUSINESS_ID FROM FAVORITES WHERE USER_NAME_ID = "
 										+ "'" + userName + "')";
-								var friends = "SELECT * FROM APP_USERS WHERE USER_NAME_ID IN ( SELECT USER_NAME_ID2 "
+								var friendsQuery = "SELECT * FROM APP_USERS WHERE USER_NAME_ID IN ( SELECT USER_NAME_ID2 "
 										+ "FROM APP_USER_FRIENDS WHERE USER_NAME_ID1 = "
 										+ "'" + userName + "')";
-
-								queryBatch.push(similarUserQuery);
-								queryBatch.push(favoriteBuziQuery);
-								queryBatch.push(friends);
 								
-								database.executeBatch(queryBatch, function(
-										errArray, resultsArray) {
-									if (errArray === null) {
-										
-										var similarUserList = rowArrayWithLabel(resultsArray[0], ['USER_NAME_ID', 'USER_NAME_ID', 'FIRST_NAME', 'LAST_NAME'], ['.url', 'user id', 'first name', 'last name']);
-										for(var i = 0; i < similarUserList.length; i++) {
-											similarUserList[i].data[0] = '/user/' + similarUserList[i].data[0];
-										}
+								async.parallel(	[
+								               	 recommend_similar_user.getTask(currentUser),
+								               	 function(callback) {
+													database.execute(favoriteBuziQuery, callback);			
+												}, 	
+												function(callback) {
+													database.execute(friendsQuery, callback);			
+												}
+										],
+										function(err, resultsArray){
+											if (err == null) {
+												var similarUserList = rowArrayWithLabel(resultsArray[0], ['USER_NAME_ID', 'USER_NAME_ID', 'FIRST_NAME', 'LAST_NAME'], ['.url', 'user id', 'first name', 'last name']);
+												for(var i = 0; i < similarUserList.length; i++) {
+													similarUserList[i].data[0] = '/user/' + similarUserList[i].data[0];
+												}
 
-										var businessList = rowArrayWithLabel(resultsArray[1], ['BUSINESS_ID',  'NAME', 'FULL_ADDRESS', 'CITY', 'STAR'], ['.url', 'name', 'address', 'city', 'star']);
-										// adjust business url format
-										for(var i = 0; i < businessList.length; i++) {
-											businessList[i].data[0] = '/business/' + businessList[i].data[0];
-										}
-										var friendsList = rowArrayWithLabel(resultsArray[2], ['USER_NAME_ID', 'FIRST_NAME', 'LAST_NAME'], ['user id', 'first name', 'last name']);
+												var businessList = rowArrayWithLabel(resultsArray[1], ['BUSINESS_ID',  'NAME', 'FULL_ADDRESS', 'CITY', 'STAR'], ['.url', 'name', 'address', 'city', 'star']);
+												// adjust business url format
+												for(var i = 0; i < businessList.length; i++) {
+													businessList[i].data[0] = '/business/' + businessList[i].data[0];
+												}
+												var friendsList = rowArrayWithLabel(resultsArray[2], ['USER_NAME_ID', 'FIRST_NAME', 'LAST_NAME'], ['user id', 'first name', 'last name']);
+												
+												var currentUserRowWithLabel = {
+														label : ['user id: ', 'name: ', 'city: ', 'state: '],
+														data : [currentUser.USER_NAME_ID, currentUser.FIRST_NAME + " " + currentUser.LAST_NAME, currentUser.LOCATION_CITY, currentUser.LOCATION_STATE]
+												};
+												
+												database_nosql.find('message', {receiver:userName}, function(results){
+													var message = rowArrayWithLabel(results, ['sender', 'text'], ['sender', 'new message']);
+													respond.render('user.jade', {
+														title : userName,
+														similar_user_list : similarUserList,
+														business_list : businessList,
+														friends_list : resultsArray[2],
+														current_user : currentUserRowWithLabel,
+														message : message
 
-										
-										var currentUserRowWithLabel = {
-												label : ['user id: ', 'name: ', 'city: ', 'state: '],
-												data : [currentUser.USER_NAME_ID, currentUser.FIRST_NAME + " " + currentUser.LAST_NAME, currentUser.LOCATION_CITY, currentUser.LOCATION_STATE]
-										};
-										
-										database_nosql.find('message', {receiver:userName}, function(results){
-											var message = rowArrayWithLabel(results, ['sender', 'text'], ['sender', 'new message']);
-											console.log(message);
-											respond.render('user.jade', {
-												title : userName,
-												similar_user_list : similarUserList,
-												business_list : businessList,
-												friends_list : resultsArray[2],
-												current_user : currentUserRowWithLabel,
-												message : message
-
-											});
+													});
+												});
+											} else {
+												console.log(err);
+											}
 										});
-									}
-								});
-
 							}
 						});
  	} else {
